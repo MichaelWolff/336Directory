e03058ebb89d7b4495bf9ac36e035a8720b352c8
Michael Wolff <mwolff@csumb.edu>
2018-03-14 15:51:45 +0000

    Got the database for Heroku working

--- Labs/4/index.php
+++ Labs/4/index.php
@@ -49,15 +49,44 @@ include 'inc/functions.php';
         }
 
         //$query = 'SELECT * FROM aTable ORDER BY '.$order;
+        //Variables used to filter the results
         $type = $_POST["type"];
         $name = $_POST["name"];
         $availability = $_POST["status"];
-        $host = "localhost";
-        $dbname = "Tester";
-        $username ="mwolff10";
-        $password = "";
+        
+        //The connection originally used in cloud9
+        /////////////////////////////////////////
+        // $host = "localhost";
+        // $dbname = "Tester";
+        // $username ="mwolff10";
+        // $password = "";
+        // $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        // $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        ////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////
+        
+        //The new connection setup for Heroku
+        ////////////////////////////////////////////////////////////
+        //$connUrl = getenv('JAWSDB_MARIA_URL');
+        $connUrl = "mysql://ikxzumlxt0a0uq9x:qendeuysn1eho7ym@thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/s1vxerk2jlp6h9j1";
+        $hasConnUrl = !empty($connUrl);
+
+        $connParts = null;
+        if ($hasConnUrl) {
+            $connParts = parse_url($connUrl);
+        }
+
+        //var_dump($hasConnUrl);
+        $host = $hasConnUrl ? $connParts['host'] : getenv('IP');
+        $dbname = $hasConnUrl ? ltrim($connParts['path'],'/') : 'Tester';
+        $username = $hasConnUrl ? $connParts['user'] : getenv('C9_USER');
+        $password = $hasConnUrl ? $connParts['pass'] : '';
+
         $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //return new PDO("mysql:host=$host;dbname=$dbname", $username, $password);//For some reason this line from Jasons exampled isnt working for me
+        //////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////
         //$sql = " SELECT * FROM device ORDER BY deviceName";//This will sort them alphabetically by the device name add DESC to reverse the order
         //$order = 'deviceName';
         $sql =  'SELECT * FROM device WHERE deviceName LIKE "'.$name.'%" AND deviceType LIKE "'.$type.'%" AND status LIKE "'.$availability.'%" ORDER BY ' .$order;
