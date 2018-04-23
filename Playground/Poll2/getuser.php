6fc3b2cb2c9ea9781a9e0b7f2342e94503bb4b1a
Michael Wolff <mwolff@csumb.edu>
2018-04-23 01:53:40 +0000

    Updated the database connection

--- Playground/Poll2/getuser.php
+++ Playground/Poll2/getuser.php
@@ -45,7 +45,7 @@
         echo "45";
         $stmt = $db->prepare($sql);
         echo "47";
-        $stmt -> execute ();//This is the problem line
+        $stmt -> execute (array(1));//This is the problem line
         echo "48";
         $tempYes = 0;
         echo "50";
