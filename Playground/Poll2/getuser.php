fafade81da51f117270fa53c0ffc0826017c8320
Michael Wolff <mwolff@csumb.edu>
2018-04-23 02:29:41 +0000

    Updated the database connection

--- Playground/Poll2/getuser.php
+++ Playground/Poll2/getuser.php
@@ -34,7 +34,7 @@
         }
         if($q == '4'){
             echo "q=4";
-            $sql =  'SELECT * FROM Poll';
+            $sql =  'SELECT * FROM `Poll` WHERE 1';
         }
         echo "45";
         $stmt = $dbConn->prepare($sql);
