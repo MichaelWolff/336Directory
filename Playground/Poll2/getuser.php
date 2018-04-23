60fa4b4d4f67b0f925463a9d06afc867cf5ded44
Michael Wolff <mwolff@csumb.edu>
2018-04-23 02:13:08 +0000

    Updated the database connection

--- Playground/Poll2/getuser.php
+++ Playground/Poll2/getuser.php
@@ -1,6 +1,5 @@
 
     <?php
-    $sql = 'SELECT * FROM Poll';
         try{
         $connUrl = getenv('JAWSDB_MARIA_URL');
         //$connUrl = "mysql://ikxzumlxt0a0uq9x:qendeuysn1eho7ym@thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/s1vxerk2jlp6h9j1";
@@ -46,9 +45,8 @@
         echo "45";
         $stmt = $db->prepare($sql);
         echo "47";
-        $stmt->execute(array( ':id' => '1'));//This is the problem line
+        $stmt -> execute (  array ( ':id' => '1')  );//This is the problem line
         echo "48";
-        //$user = $query->fetch(PDO::FETCH_ASSOC);
         $tempYes = 0;
         echo "50";
         $tempNo = 0;
