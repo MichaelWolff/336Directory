816c3bc617d503231d75078079700dd74e157de0
Michael Wolff <mwolff@csumb.edu>
2018-04-23 01:40:03 +0000

    Updated the database connection

--- Playground/Poll2/getuser.php
+++ Playground/Poll2/getuser.php
@@ -44,6 +44,7 @@
         }
         echo "45";
         $stmt = $db->prepare($sql);
+        echo "47";
         $stmt -> execute (  array ( ':id' => '1')  );//This is the problem line
         echo "48";
         $tempYes = 0;
