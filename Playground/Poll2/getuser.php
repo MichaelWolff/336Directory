942629058ebc83b4493ceb9a5e3f29453a91ed00
Michael Wolff <mwolff@csumb.edu>
2018-04-23 02:24:42 +0000

    Updated the database connection

--- Playground/Poll2/getuser.php
+++ Playground/Poll2/getuser.php
@@ -1,7 +1,6 @@
 
     <?php
     $sql = 'SELECT * FROM Poll';
-        try{
         $connUrl = getenv('JAWSDB_MARIA_URL');
         //$connUrl = "mysql://ikxzumlxt0a0uq9x:qendeuysn1eho7ym@thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/s1vxerk2jlp6h9j1";
         $hasConnUrl = !empty($connUrl);
@@ -17,14 +16,8 @@
         $username = $hasConnUrl ? $connParts['user'] : getenv('C9_USER');
         $password = $hasConnUrl ? $connParts['pass'] : '';
 
-        $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "connected succesfully";
-        }
-        
-        catch(PDOException $e){
-            echo "Connection failed: ". $e->getMessage();
-        }
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
         echo "This is a test";
         //$q=4;
